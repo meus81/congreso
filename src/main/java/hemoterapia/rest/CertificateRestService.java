5b37c268910209ac5a3d2abf0d4665dc23453c7c
Marcos <pfframes@yahoo.com.ar>
2015-09-29 13:45:37 -0300

    Certificate service

--- src/main/java/hemoterapia/rest/CertificateRestService.java
+++ src/main/java/hemoterapia/rest/CertificateRestService.java
@@ -1,27 +1,24 @@
 package hemoterapia.rest;
 
-import java.util.List;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 
 import hemoterapia.domain.Certificate;
-import hemoterapia.domain.Person;
 import hemoterapia.services.CertificateService;
-import hemoterapia.services.PersonService;
 
 @ApplicationPath("/resources")
 @Path("/")
 public class CertificateRestService {
+	
 	@GET
-	@Path("/certifiacte/{id}")
+	@Path("/certificate/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Certificate getCertificate(@PathParam("id") int id ){
+		System.out.println("El id del parametro " + id);
 		CertificateService personService = new CertificateService();
 		return personService.getCertificate(id);
 	}
--- src/main/java/hemoterapia/services/CertificateService.java
+++ src/main/java/hemoterapia/services/CertificateService.java
@@ -3,21 +3,24 @@ package hemoterapia.services;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 import hemoterapia.domain.Certificate;
 
 public class CertificateService {
-	
+
 	private static final String PERSISTENCE_UNIT_NAME = "congreso";
 	private static EntityManagerFactory factory;
-	
-	public Certificate getCertificate(int id){
+
+	public Certificate getCertificate(int id) {
 		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	    EntityManager em = factory.createEntityManager();
-		return (Certificate)em.createQuery("SELECT c FROM certificate c WHERE c.idCertificate = :id")
-				 			  .setParameter(":id", id)
-				              .getResultList()
-				              .get(0);
+		EntityManager em = factory.createEntityManager();
+		Certificate certificate = (Certificate) em
+				.createQuery("SELECT c FROM Certificate c WHERE c.idCertificate = :id")
+				.setParameter("id", id)
+				.getSingleResult();
+
+		System.out.println("El certificado obtenido es: " + certificate.toString() + " - Tax: " + certificate.getTax());
+
+		return certificate;
 	}
 }
