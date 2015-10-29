dd511d1ae5210539c6e9552038fbb0b5b450c8e0
Marcos Urbaneja <marcos@brightsector.com>
2015-10-29 01:28:29 -0300

    va queriendo falta poquito

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -48,11 +48,14 @@ app.directive("mySearchResults", function(){
 			tablesdata:'=',
 			addmodify:'&'
 		},
-		templateUrl: "partials/search-results-directive.html",
-		controller: function ($scope, $attrs){
-			$scope.modifyline= function(){
+		templateUrl:"partials/search-results-directive.html",
+		controller:function ($scope, $attrs){
+			console.log("Hola que tal");
+		
+			$scope.modifyline = function (tabledata){
 				console.log("Dir - en el controlador invocando a modificar");
-				$scope.addmodify();
+				console.log("el valor de tabledata es: " + tabledata);
+				$scope.addmodify({tabledata:tabledata});
 			}
 		}
 	};
@@ -72,6 +75,8 @@ app.directive("myTableResults", function(){
 app.controller('SearchPersonController', [ '$scope', 'PersonFactory',
                                            'searchService', function($scope, PersonFactory, searchService) {
 		
+	$scope.editingData = {};
+	
 	$scope.getPersons = function(person) {
 		console.log(person.name);
 		console.log(person.surname);
@@ -79,37 +84,36 @@ app.controller('SearchPersonController', [ '$scope', 'PersonFactory',
 			name : person.name,
 			surname : person.surname
 		});
+	
 		searchService.setResults(result);
 	};
 	$scope.resetForm = function() {
 		console.log("Ctrl - Borrando formulaio");
         $scope.person.name = '';  
         $scope.person.surname = '';
+        $scope.editingData = {};
     };
     
     $scope.getResultsForTable=function(){
     	console.log ("Ctrl - Obteniendo resultados para la tabla");
-    	return searchService.getResults();
+    	
+    	$scope.tablesdata = searchService.getResults();
+    	for (var i = 0, length = $scope.tablesdata.length; i < length; i++) {    		
+    		$scope.editingData[$scope.tablesdata[i].idPerson]= false;
+    	}
+   
+    	console.log("que carajo sos editingData?? " + $scope.editingData);
+    	return $scope.tablesdata;
     };
     
-    $scope.prueba = function(){
-    	console.log("Ctrl - ingresando a prueba");
-    };   
-    
-    $scope.setModifyButton= function(id){
+    $scope.setModifyButton= function(data){
     	console.log("Ctrl - Seteando resultados para la tabla");
-		$scope.tablesdata = searchService.results;
-		
-//		$scope.editingData = [];
-//		for (var i = 0, length = $scope.tablesdata.length; i < length; i++) {
-//			$scope.editingData[$scope.tablesdata[i].id] = false;
-//		}
-//		$scope.modify = function(tableData) {
-//			$scope.editingData[tableData.id] = true;
-//		};
-//		$scope.update = function(tableData) {
-//			$scope.editingData[tableData.id] = false;
-//		};
+    	console.log("que es tabledata: " + data)
+		$scope.editingData[data.idPerson] = true
+    };
+
+    $scope.update = function(data) {
+    	$scope.editingData[data.idPerson] = false;
     }
 } ]);
 
@@ -147,3 +151,64 @@ app.controller('SearchPersonController', [ '$scope', 'PersonFactory',
 // });
 // }
 // }]);
+
+//app.directive('myNotebook', function ($log) {
+//    return {
+//        restrict:"E",
+//        scope:{
+//            notes:'=',
+//            ondelete:'&'
+//        },
+//        templateUrl:"partials/ejemplo.html",
+//        controller:function ($scope, $attrs) {
+//        	console.log("antes del deleteNotedirective");
+//        	var algo = $scope
+//            $scope.deleteNoteDirective = function (id) {
+//            	console.log("entre a delete Note Directive");
+//                $scope.ondelete({id:id});
+//            }
+//        }
+//    };
+//})
+//app.directive('myNote', function () {
+//    return {
+//        restrict:'E',
+//        scope:{
+//            delete:'&',
+//            note:'='
+//        },
+//        link:function (scope, element, attrs) {
+//            // Since this project pulls in jQuery, element is already
+//            // a wrapped jQuery object, so there is no need to do 
+//            // something like:  var $el = $(element);
+//            // We can use element directly with jQuery methods.
+//
+//        	console.log(element);
+//        	 $(element).hide().fadeIn('slow');
+
+//            $('.thumbnails').sortable({
+//                placeholder:"ui-state-highlight", forcePlaceholderSize:true
+//            });
+//        }, 
+//    };
+//})
+//app.controller('NotebookCtrl', ['$scope', 'notesService', function ($scope, notesService) {
+//$scope.getNotes = function () {
+//    return notesService.notes();
+//};
+//
+//$scope.addNote = function (noteTitle) {
+//	console.log("agrego nota");
+//    if(noteTitle != '') {
+//        notesService.addNote(noteTitle);
+//    }
+//};
+//
+//$scope.deleteNote = function (id) {
+//    notesService.deleteNote(id);
+//};
+//
+//$scope.resetForm = function() {
+//    $scope.noteTitle = '';
+//};
+//}]);
--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -67,3 +67,36 @@ function resourceResponseHandler(response){
 //	      return $http({ method: 'POST', url: '/CongresoHemoterapia/rest/save_person', data: person});
 //	  }
 //}]);
+
+//EJEMPLO
+//services.service('notesService', function () {
+//	console.log("en el servicio");
+//    var data = [{id:1, title:'Note 1'},
+//                {id:2, title:'Note 2'},
+//                {id:3, title:'Note 3'},
+//                {id:4, title:'Note 4'},
+//                {id:5, title:'Note 5'},
+//                {id:6, title:'Note 6'},
+//                {id:7, title:'Note 7'},
+//                {id:8, title:'Note 8'}];
+//
+//    return {
+//        notes:function () {
+//            return data;
+//        },
+//        addNote:function (noteTitle) {
+//            var currentIndex = data.length + 1;
+//            data.push({
+//                id:currentIndex, title:noteTitle
+//            });
+//        },
+//        deleteNote:function (id) {
+//            var oldNotes = data;
+//            data = [];
+//
+//            angular.forEach(oldNotes, function (note) {
+//                if (note.id !== id) data.push(note);
+//            });
+//        }
+//    };
+//})
